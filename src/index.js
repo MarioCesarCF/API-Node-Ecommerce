a31b420ca331de8058293fe7815f201950dedc5a
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-31 15:49:43 -0300

    update package.json

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 const index = require('./routers/index');
 const productRoute = require('./routers/productRoute');
 
