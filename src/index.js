cdf7469a1588547602e7f7be3fc0162757945d88
DJehSantana <jessicah.dias23@hotmail.com>
2022-10-13 13:52:44 -0300

    incluindo o cors nas configurações do express

--- src/index.js
+++ src/index.js
@@ -18,18 +18,6 @@ const cors = require('cors');
 const jwt = require('./middlewares/jwt');
 const app = express();
 
-
-app.use(cors());
-
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
 const index = require('./routers/index');
 const productRoute = require('./routers/productRoute');
 const conection = require('./config/conection');
@@ -45,10 +33,21 @@ const logger = require('./middlewares/logger');
 
 //método que configura o express
 const configurarExpress = () => {
+
     app.use(logger);
     
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
+
+    app.use(cors());
+    
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", '*');
+        res.header("Access-Control-Allow-Credentials", true);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+        next();
+    });
     
     app.use(jwt);
 
